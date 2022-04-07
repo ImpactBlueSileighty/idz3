2e4750f465fcb3316aa01b73a70321870cd806b2
Igor <72380374+ImpactBlueSileighty@users.noreply.github.com>
2022-04-07 09:53:52 +0300

    Update source.c

--- source.c
+++ source.c
@@ -1,64 +1,70 @@
 #include <stdio.h>
-#include <locale.h>
+
+#define WIDTH 5
+#define HEIGHT 5
 
 int main()
 {
-    setlocale(0, "");
+    
     FILE *file;
-    int array[5][5];
+    int array[WIDTH][HEIGHT];
     int size = 0;
     int i = 0;
-    int j = 0;
-    int edges = 0;
+    int j = 0; //вершины
+    int edges = 0; //ребра
 
     if ((file = fopen("input.txt", "r")) == NULL)
     {
-        printf("\noFile not found.\n");
+        puts("\noFile not found.\n");
         return 1;
     }
 
-    for (int k = 0; k < 5; k++)
+    for (int k = 0; k < HEIGHT; k++)
     {
 
-        for (int l = 0; l < 5; l++)
+        for (int l = 0; l < WIDTH; l++)
         {
             fscanf(file, "%d", &array[k][l]);
-        }
-    }
-    for (int k = 0; k < 5; k++)
-    {
-        for (int l = 0; l < 5; l++)
             printf("%d ", array[k][l]);
-        printf("\n");
+        }
+        putchar('\n');
     }
 
     fclose(file);
 
     FILE *out = fopen("output.dot", "w");
 
-    fprintf(out, "graph{\n");
+    fputs("graph{", out);
 
-    for (i; i < 5; i++)
+    for (int k = 0; k < WIDTH; k++)
     {
-        for (j = i; j < 5; j++)
+        fprintf(out, "%d;\n", k);
+    }
+
+    for (i; i < HEIGHT; i++)
+    {
+        for (j = i; j < WIDTH; j++)
         {
-            if (array[i][j] >= 1)
+            if (array[i][j] >= 1 && i != j)
             {
                 fprintf(out, "%d -- %d;\n", i, j);
                 edges++;
             }
         }
     }
-    fprintf(out, "}");
+    fputs("}", out);
 
     fclose(out);
 
-    if(edges > (( j - 1 )*( j - 2 )/ 2)){
+    if(edges >= (( j - 1 )*( j - 2 )/ 2)){
         printf("This is a linked graph. Edges: %d, peaks: %d \n", edges, j);
-    }else{
-        printf("This is an unrelated graph. Edges: %d, peaks: %d \n", edges, j);
+    }
+    else
+    {
+        printf("This is an unlinked graph. Edges: %d, peaks: %d \n", edges, j);
     }
 
     system("dot output.dot -Tpng -o image.png");
     return 0;
 }
+
